23530a2c0eba02bf95a1f54680d250b716e3f941
Iain Calder <iain.calder@gmail.com>
2025-12-28 18:19:04 +0000

    chore: run autoUpgrade on Sundays at 2am

--- modules/autoupgrade.nix
+++ modules/autoupgrade.nix
@@ -3,7 +3,7 @@
   system.autoUpgrade = {
     enable = true;
     flake = "github:icalder/nixos#${config.networking.hostName}"; # Point to your central repo
-    dates = "02:00"; # Run at 2 AM
+    dates = "Sun 02:00"; # Run at 2 AM on Sunday, after CI has updated flake.lock
     randomizedDelaySec = "45min"; # Avoid all Pis hitting GitHub at once
     flags = [
       "--refresh" # Force fetching latest flake info
