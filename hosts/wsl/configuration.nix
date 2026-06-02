5273bcc11f267518bab9a6d17c50cef508f84afc
Iain Calder <iain.calder@gmail.com>
2026-06-02 20:34:42 +0000

    chore: bump llama.cpp to b9481

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -23,11 +23,11 @@ let
       metalSupport = false;
     }).overrideAttrs
       (oldAttrs: rec {
-        version = "9460";
+        version = "9481";
         src = unstable-pkgs.fetchFromGitHub {
           inherit (oldAttrs.src) owner repo;
           tag = "b${version}";
-          hash = "sha256-N9sswjopH31WED/pTcNiJdikX1CYglm34UiRT16Lifg=";
+          hash = "sha256-jwwsQNZs+XxGRG/DeR523mCORl/nxiVv6kuacHqdorM=";
         };
         npmRoot = "tools/ui";
         npmDepsHash = "sha256-Iyg8FpcTKf2UYHuK7mA3cTAqVaLcQPcS0YCa5Qf01Gc=";
